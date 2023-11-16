00b8ddd54c5e3010baf77e875dd78fabf9f14a12
Alexandr Tarasiuk <46819896+monstrikwho@users.noreply.github.com>
2023-11-16 21:34:02 +0300

    Добавил остальные поля

--- src/components/SignupForm/index.vue
+++ src/components/SignupForm/index.vue
@@ -5,12 +5,63 @@
     <InputField
       fieldType="login"
       fieldStatus=""
-      fieldMessage=""
+      :fieldMessage="formErors.login"
       placeholder="Логин"
+      @update="update($event)"
     />
-
+    <InputField
+      fieldType="email"
+      fieldStatus=""
+      :fieldMessage="formErors.email"
+      placeholder="Email"
+      @update="update($event)"
+    />
+    <InputField
+      fieldType="password"
+      fieldStatus=""
+      :fieldMessage="formErors.password"
+      placeholder="Пароль"
+      @update="update($event)"
+    />
+    <InputField
+      fieldType="confirmPassword"
+      fieldStatus=""
+      :fieldMessage="formErors.confirmPassword"
+      placeholder="Подтвердите пароль"
+      @update="update($event)"
+    />
     <div class="actions">
       <a class="button disabled">Зарегистрироваться</a>
     </div>
   </div>
 </template>
+
+<script>
+import InputField from "./InputField.vue";
+
+export default {
+  components: { InputField },
+  data() {
+    return {
+      formData: {
+        login: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      },
+      formErors: {
+        login: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      },
+      invalid: true,
+    };
+  },
+  methods: {
+    update(event) {
+      this.formData[event.type] = event.value;
+    },
+  },
+};
+</script>
